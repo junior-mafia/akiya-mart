2a186c6ed6f5933571c4d51926f8732f26a3a9d3
Joey Stox <joestox@gmail.com>
2023-05-14 13:54:39 -0300

    update dependencies

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import RangeSlider from './RangeSlider'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
 import { createHash } from 'crypto'
